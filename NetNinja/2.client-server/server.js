b5060b00a4233ca7ea0660e7823ef29fb6bf8eeb
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-11-29 13:59:55 +0700

    :package: add index.html file

--- NetNinja/2.client-server/server.js
+++ NetNinja/2.client-server/server.js
@@ -1,7 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made.');
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('<h2>Hello and Welcome!</h2>');
+    // res.end;
+
+    fs.readFile('./index.html', (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else{
+            // res.write(data);
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
