13ef06489350be9990464bada5d36eb8ac6a525e
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-11-30 11:16:05 +0700

    :hammer: update app.js (redirect and 404)

--- nodejs/2.client-server/app.js
+++ nodejs/2.client-server/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.listen(3000);
 
+// listen for request
 app.get('/', (req, res) => {
     // res.send('<p>Send Nudes</p>');
     res.sendFile('./html/index.html', {root: __dirname});
@@ -13,4 +14,15 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     // res.send('<p>About to Send Nudes</p>');
     res.sendFile('./html/about.html', {root: __dirname});
+});
+
+// redirects
+app.get('about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404 redirects
+// must be at the bottom so that if the rest of the path doesn't match, then it will return this 404 file.
+app.use((req, res) => {
+    res.sendFile('./html/404.html', {root: __dirname});
 });
