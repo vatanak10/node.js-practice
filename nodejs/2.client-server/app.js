d0040455fecae54db72a73640077bc5f2900ad39
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-12-01 14:59:36 +0700

    :sparkles: connect mongoose

--- nodejs/2.client-server/app.js
+++ nodejs/2.client-server/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
+
 // CONNECT TO MONGODB
-const dburi = 'mongodb+srv://Arzel:supervampire@nodejs-practice.oru6o.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const dburi = 'mongodb+srv://Arzel:supervampire@nodejs-practice.oru6o.mongodb.net/testing?retryWrites=true&w=majority';
+mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+      console.log('connected to database ');
+      app.listen(3000);
+      console.log('lisening for request...');
+    })
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+// app.listen(3000);
 
 // Middleware
 /*
