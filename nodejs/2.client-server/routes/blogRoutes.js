5a84f2ebb2f285d5c99ddabeda77856a639fe2b9
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-12-03 20:09:39 +0700

    :sparkles: MVC Controller

--- nodejs/2.client-server/routes/blogRoutes.js
+++ nodejs/2.client-server/routes/blogRoutes.js
@@ -1,60 +1,12 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
-// BLOG routes
-router.get('/', (req,res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', {title: 'All Blogs', blogs: result});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-
-router.post('/', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result)=> {
-            res.redirect('/blogs');
-            console.log('blog success...');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-
-router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create Blog'});
-});
-
-router.get('/:id', (req, res)=> {
-    const id = req.params.id;
-    // console.log(id);
-    Blog.findById(id)
-        .then(result => {
-            res.render('single-blog', {blog: result, title: 'Blog Details'});
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-router.delete('/:id', (req,res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs'});
-        })
-        .catch(err => console.log(err));
-});
-
-
+router.get('/', blogController.blog_index);
+router.get('/create', blogController.blog_create_get);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_detail);
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
