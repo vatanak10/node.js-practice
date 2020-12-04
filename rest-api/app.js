7b0909b0dda9ddefb0a9d7a7922498c3fe28d738
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-12-04 19:28:05 +0700

    :sparkles: handling requests

--- rest-api/app.js
+++ rest-api/app.js
@@ -5,3 +5,8 @@ const app = express();
 app.listen(process.env.port || 3000, function(){
     console.log('ready for request...');
 });
+
+app.get('/', (req, res) => {
+    console.log('GET request...');
+    res.send({name: 'Arzel'});
+});
