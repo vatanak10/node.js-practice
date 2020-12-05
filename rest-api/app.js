7669ebb8270c1e981cb6fb1543ea70e0e55ff450
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-12-05 13:38:31 +0700

    :sparkles: creating routes

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const router = require('./routes/api');
 
 const app = express();
+app.use('/api', router);
 
 app.listen(process.env.port || 3000, function(){
     console.log('ready for request...');
---
+++ rest-api/routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/post', (req, res)=>{
+    res.send({type:'GET'});
+});
+
+router.post('/post', (req, res)=>{
+    res.send({type:'POST'});
+});
+
+router.put('/post/:id', (req, res)=>{
+    res.send({type:'PUT'});
+});
+
+router.delete('/post/:id', (req, res)=>{
+    res.send({type:'DELETE'});
+});
+
+module.exports = router;
