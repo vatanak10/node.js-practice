c2531248eee35d4d63acea51e8843a4ebfd8b735
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2020-12-17 14:19:22 +0700

    :sparkles: add error messages

--- jwt/controllers/authController.js
+++ jwt/controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 const signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,8 +8,16 @@ const login_get = (req, res) => {
     res.render('login');
 }
 
-const signup_post = (req, res) => {
-    res.send('new user');
+const signup_post = async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    } catch(err){
+        console.log(err);
+        res.status(400).send('error, user not created');
+    }
 }
 
 const login_post = (req, res) => {
