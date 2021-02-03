929eadb505937cd724da5d40a4d5701786dafdcb
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-02-03 15:57:36 +0700

    :hammer: read cookies

--- jwt/app.js
+++ jwt/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -24,6 +26,14 @@ app.use(authRoutes);
 
 //cookies
 app.get('/set-cookies', (req, res) => {
-  res.setHeader('Set-Cookie', 'newUser=true');
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false, { maxAge: 1000*60*60*24 }); // maxAge: set duration of the cookie to be exist, httpOnly, secure
   res.send('You got new cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
 });
